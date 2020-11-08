2a6fedc2d69c5a98b7d8a6377bd059f038251ff4
GardaAlex <aleex.dorin@gmail.com>
2020-11-08 21:28:47 +0200

    Manage errors

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -1,10 +1,33 @@
 package org.fasttrackit;
 
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main( String[] args ){
+
+        Engine daciaEngine = new Engine();
+        daciaEngine.setManufacturer("Renault");
+
+        RadioControlledCar dacia = new RadioControlledCar(daciaEngine);
+        dacia.setName("Dacia");
+        dacia.setColor("Red");
+        dacia.setDoorCount(4);
+        dacia.setWheelCount(4);
+        dacia.setMileage(8.5);
+
+        Engine ladaEngine = new Engine();
+        ladaEngine.setManufacturer("Lada");
+
+        RadioControlledCar lada = new RadioControlledCar(ladaEngine);
+        lada.setColor("Blue");
+        lada.setName("Lada");
+        lada.setDoorCount(2);
+        lada.setWheelCount(4);
+        lada.setMileage(3.2);
+
+        RadioControlledMotorcycle motorcycle = new RadioControlledMotorcycle(new Engine());
+
+        Race race = new Race(dacia, motorcycle);
+
+
     }
 }
---
+++ src/main/java/org/fasttrackit/Race.java
@@ -0,0 +1,21 @@
+package org.fasttrackit;
+
+public class Race {
+
+    private Vehicle firstCompetitor;
+    private Vehicle secondCompetitor;
+
+    public Race(Vehicle firstCompetitor, Vehicle secondCompetitor) {
+        this.firstCompetitor = firstCompetitor;
+        this.secondCompetitor = secondCompetitor;
+    }
+
+    public Vehicle getFirstCompetitor() {
+        return firstCompetitor;
+    }
+
+    public Vehicle getSecondCompetitor() {
+        return secondCompetitor;
+    }
+}
+
